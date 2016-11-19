4396102f541aca2ce3bbf032e71af4d1978d77a1
Kicking6 <rorywadeallen@gmail.com>
2016-11-20 12:10:36 +1300

    Set up of basic file parameters
    Put basic structure into the file and created a sample file containing
    some structure that can be used to test the code during production

---
+++ Main.py
@@ -0,0 +1,18 @@
+import yaml
+
+def file_reader(filename):
+	file = open(filename, "r")
+	contents = yaml.load(file) # this will change depending on file type
+	print(contents)
+	print("Not done yet")
+	
+def user_input():
+	print("Not done yet")
+	
+def main():
+	#filename = input("Enter filename: ")
+	filename = "Pratice data.yaml"
+	file_reader(filename)
+	print("Not done yet")
+
+main()
