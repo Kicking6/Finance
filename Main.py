3d770875bc9b6e923749c318e26ce6cd13fae673
Evander7 <jjemace@gmail.com>
2016-11-30 16:00:36 +1300

    Added database
    Added the 'transactions.db' database, and added a small
    add_transaction() function that allows you to enter in a new transaction
    and save it to the database.
    This is not intended for production, it's more a proof of concept.
    
    Disclaimer: I don't know if this actually works lol.

--- Main.py
+++ Main.py
@@ -2,17 +2,35 @@ import sqlite3
 
 def file_reader(filename):
 	file = open(filename, "r")
-	contents = yaml.load(file) # this will change depending on file type
-	print(contents)
+	#contents = yaml.load(file) # this will change depending on file type
+	#print(contents)
 	print("Not done yet")
-	
+
 def user_input():
 	print("Not done yet")
-	
+
+
+def add_transaction():
+	# Connects to the database
+	conn = sqlite3.connect("transactions.db")
+	c = conn.cursor()
+
+	print("Entering a transaction: \n")
+	amount = input("Please enter the amount: ")
+	cat = input("Under what category does this fall?")
+	date = input("What date did this occur?")
+	i_or_out = input("Was this an inflow or outflow?")
+	c.execute("INSERT INTO transactions (amount, category, date, inflow_or_outflow) VALUES (?,?,?,?);", (amount, cat, date, i_or_out))
+
+	# Commits the changes
+	conn.commit()
+	c.close()
+
 def main():
+	add_transaction()
 	#filename = input("Enter filename: ")
 	filename = "Pratice data.yaml"
-	file_reader(filename)
+	#file_reader(filename)
 	print("Not done yet")
 
 main()
