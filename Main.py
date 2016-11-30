2df0faf6c6f580b8e7481b3b92aea2c37b565a44
Evander7 <jjemace@gmail.com>
2016-11-30 16:21:09 +1300

    Added some GUI
    Added a small GUI for some parts. Again, just a work in progress.
    GUI for the adding transaction still needs a lot of work, like
    validating the input and checking for errors in peoples' input.

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import sqlite3
+import easygui as EG
 
 def file_reader(filename):
 	file = open(filename, "r")
@@ -11,23 +12,47 @@ def user_input():
 
 
 def add_transaction():
+	msg = "Please enter the details of this transaction"
+	title = "Rory's Financerifier"
+	fields = ["Amount", "Category", "Date", "Inflow or Outflow?"]
+
+
+	transactionValues = EG.multenterbox(msg, title, fields)
+
 	# Connects to the database
 	conn = sqlite3.connect("transactions.db")
 	c = conn.cursor()
-
-	print("Entering a transaction: \n")
-	amount = input("Please enter the amount: ")
-	cat = input("Under what category does this fall?")
-	date = input("What date did this occur?")
-	i_or_out = input("Was this an inflow or outflow?")
-	c.execute("INSERT INTO transactions (amount, category, date, inflow_or_outflow) VALUES (?,?,?,?);", (amount, cat, date, i_or_out))
+	c.execute("INSERT INTO transactions (amount, category, date, inflow_or_outflow) VALUES (?,?,?,?);", tuple(transactionValues))
 
 	# Commits the changes
 	conn.commit()
 	c.close()
 
+
+# Creates the Gui. what else is there to say?
+def drawGui():
+	mainMenuChoices = ["View Account Summary", "Add a Transaction"]
+	title = "Rory's Financerifier"
+	msg = "Welcome to the main menu. What would you like to do?"
+	# This returns the index of what the user chose. eg, 0 for acc summary, 1 for adding transaction
+	mainMenuNavChoice = EG.indexbox(msg, title, mainMenuChoices)
+	if mainMenuNavChoice == 0:
+		account_summary_GUI()
+	elif mainMenuNavChoice == 1:
+		add_transaction()
+	else:
+		pass
+
+# Draws the GUI for the account summary.
+# Maybe you want to get the account summary first? Maybe this calls that function? Just a placeholder for now.
+def account_summary_GUI():
+	print("Not dont yet")
+
+
+
 def main():
-	add_transaction()
+	drawGui()
+	# add_transaction()
 	#filename = input("Enter filename: ")
 	filename = "Pratice data.yaml"
 	#file_reader(filename)
